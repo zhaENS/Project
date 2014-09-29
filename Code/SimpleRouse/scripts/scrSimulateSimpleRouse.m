2bc0273f3d0146b6a07e1218899c151b77b800bd
ofirEns <ofirshukron@gmail.com>
2014-09-29 10:41:58 +0200

    changed simple rouse to account for 2 TADs simulations

--- Code/SimpleRouse/scripts/scrSimulateSimpleRouse.m
+++ Code/SimpleRouse/scripts/scrSimulateSimpleRouse.m
@@ -17,4 +17,7 @@ hour   = num2str(c(4));
 minute = num2str(c(5));
 resultsFolder = fullfile(pwd,'..','..','PolymerChainDynamicsResults');
 saveName = sprintf('%s',['simpleRouse_',rp.recipeFileName,'_',hour,'_', minute,'_',d,'.mat']);
+
+% Remove bead distance and save
+srf.beadDistance = [];
 save(fullfile(resultsFolder,saveName),'srf')
