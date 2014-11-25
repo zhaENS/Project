f9000af9f5978a053887d7c148210cd37cf0c0b2
ofirEns <ofirshukron@gmail.com>
2014-11-25 10:41:17 +0100

    changed parameters to run simulations with one TAD and calculate the MSD

--- Code/SimpleRouse/SimpleRouseParams.m
+++ Code/SimpleRouse/SimpleRouseParams.m
@@ -1,7 +1,7 @@
 function rouseParams        = SimpleRouseParams
 % Parameters for the SimpleRouse class and simulations
 rouseParams.numRounds       = 1; % number of simulation rounds 
-rouseParams.numSimulations  = 10000; % number of simulations in each round
+rouseParams.numSimulations  = 5000; % number of simulations in each round
 rouseParams.dimension       = 3;
 rouseParams.numBeads        = 307;
 rouseParams.b               = 1;
@@ -20,7 +20,7 @@ rouseParams.affineBeadsNum  = [];    % a fixed pair indices of affine beads
 rouseParams.kOff            = 0.1;   % the detachment rate of affine beads
 rouseParams.beta            = 2;     % ~~!!for beta~=2 the polymer is a beta polymer!!~~to set all betas to the same value insert one value only
 rouseParams.recipeFolder    = fullfile(pwd,'SimpleRouse','Recipes');
-rouseParams.recipeFileName  = 'simpleRouseSimulateTwoTADs';%'simpleRouseSimulateMeanFirstEncounterEndToEnd';
+rouseParams.recipeFileName  = 'simpleRouseSimulateOneTADwithTail';%'simpleRouseSimulateMeanFirstEncounterEndToEnd';
 rouseParams.defaultRecipe   = 'simpleRouseDebugRecipe'; % default recipe file name
 rouseParams.saveBeadDist    = 'last'; % [last/current/all/meanSquare] ( note that only for 'last' and 'all' the encounters can reliably be calculated)
 rouseParams.calculateMSD    = false;  % indicate whether to calculate the MSD for each bead (slows down simulations)
