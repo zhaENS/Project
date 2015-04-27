43847a6963e46ad6dbf43b331f3dc06ddafc0518
ofirEns <ofirshukron@gmail.com>
2015-04-27 20:42:29 +0200

    benchmarks on the code

--- Code/Framework/SimulationFrameworkParams.m
+++ Code/Framework/SimulationFrameworkParams.m
@@ -25,11 +25,11 @@ classdef SimulationFrameworkParams<handle
             obj.simulator.runSimulation        = false; % a flag indicating whether to allow the simulation to run at initiation 
             obj.simulator.numSimulationBatches = 1;     % number of simulation batches
             obj.simulator.numSimulations       = 1;     % number of simulations in each batch
-            obj.simulator.numSteps             = Inf;   % for inf place Inf
+            obj.simulator.numSteps             = 1000;   % for inf place Inf
             obj.simulator.dt                   = 1e-2;  % time step 
-            obj.simulator.numChains            = 10;   
+            obj.simulator.numChains            = 1;   
             obj.simulator.encounterDist        = 0.1;   % The distance for which two monomer are considered to have met 
-            obj.simulator.showSimulation       = true; 
+            obj.simulator.showSimulation       = false; 
             obj.simulator.recordData           = false;
             obj.simulator.notifyByEmail        = false;
             obj.simulator.notifyCycleLength    = 32;    % number of simulation cycles after which an email is sent 
