1f5a64ff261c01908b8700cc64499de7c038759e
ofirENS <ofirshukron@gmail.com>
2015-06-16 23:41:49 +0200

    changed the bending function to the correct form (hopefully)

--- Code/TestCodes/BendingElasticityWithAngels.m
+++ Code/TestCodes/BendingElasticityWithAngels.m
@@ -13,13 +13,18 @@ cosTheta  = @(pos,dist,i) ((1./(dist(i,i+1)*dist(i+1,i+2)))*sum((pos(i,:)-pos(i+
 %                 (pos(i+1,:)-pos(i,:)).*cosTheta(pos,dist,i)./dist(i,i+1));% d cos(theta_i)/dr_i
 % F3        = @(pos,dist,i)  -(F1(pos,dist,i)-F2(pos,dist,i));
 
-% before 
-F1          = @(pos,dist,i)(pos(i-2,:)-pos(i-1,:))./(dist(i-2,i-1)*dist(i-1,i));
-% during 
-F2          = @(pos,dist,i)(2*pos(i,:)-pos(i-1,:)-pos(i+1,:))./(dist(i,i-1)*dist(i,i+1));
-% after 
-F3          = @(pos,dist,i)(pos(i+2,:)-pos(i+1,:))./(dist(i,i+1)*dist(i+1,i+2));
+F1 = @(pos,dist,i,cosTheta0)((pos(i-2,:)-pos(i-1,:))*(pos(i,:)-pos(i-1,:))'./(dist(i-2,i-1)*dist(i-1,i)) -cosTheta0)*(pos(i-2,:)-pos(i-1,:))./(dist(i-2,i-1)*dist(i,i-1));
+F2 = @(pos,dist,i,cosTheta0)((pos(i-1,:)-pos(i,:))*(pos(i+1,:)-pos(i,:))'./(dist(i-1,i)*dist(i+1,i)) -cosTheta0)* (2*pos(i,:)-pos(i-1,:)-pos(i+1,:))./(dist(i-1,i)*dist(i+1,i));
+F3 = @(pos,dist,i,cosTheta0)((pos(i+2,:)-pos(i+1,:))*(pos(i,:)-pos(i+1,:))'./(dist(i+2,i+1)*dist(i+1,i)) -cosTheta0)* (pos(i+2,:)-pos(i+1,:))./(dist(i+2,i+1)*dist(i,i+1)); 
 
+% % before 
+% F1          = @(pos,dist,i)(pos(i-2,:)-pos(i-1,:))./(dist(i-2,i-1)*dist(i-1,i));
+% % during 
+% F2          = @(pos,dist,i)(2*pos(i,:)-pos(i-1,:)-pos(i+1,:))./(dist(i,i-1)*dist(i,i+1));
+% % after 
+% F3          = @(pos,dist,i)(pos(i+2,:)-pos(i+1,:))./(dist(i,i+1)*dist(i+1,i+2));
+pos = particlePosition;
+dist = particleDistance;
 
 bAngle    = cos(angle0);
 
@@ -33,25 +38,29 @@ for pIdx = 1:numParticles
 %                                       (cosTheta(particlePosition,particleDistance,pIdx-1)- bAngle)*F3(particlePosition,particleDistance,pIdx-1)*inds(3));
 %         
     if pIdx ==1                
-        force(pIdx,:) = bendingConst*(cosTheta(particlePosition,particleDistance,pIdx)-bAngle)*...
-                                   F3(particlePosition,particleDistance,pIdx);
+%         force(pIdx,:) = bendingConst*(cosTheta(particlePosition,particleDistance,pIdx)-bAngle)*...
+%                                    F3(particlePosition,particleDistance,pIdx);
 %                                     F2(particlePosition,particleDistance,pIdx);
+         force(pIdx,:) = bendingConst*F3(pos,dist,1,bAngle);
     elseif pIdx == 2 && (pIdx~=(numParticles-1));
         
-        force(pIdx,:) = bendingConst*((cosTheta(particlePosition,particleDistance,pIdx)-bAngle)*F3(particlePosition,particleDistance,pIdx)+...
-                                      (cosTheta(particlePosition,particleDistance,pIdx-1)-bAngle)*F2(particlePosition,particleDistance,pIdx));
+%         force(pIdx,:) = bendingConst*((cosTheta(particlePosition,particleDistance,pIdx)-bAngle)*F3(particlePosition,particleDistance,pIdx)+...
+%                                       (cosTheta(particlePosition,particleDistance,pIdx-1)-bAngle)*F2(particlePosition,particleDistance,pIdx));
+         force(pIdx,:) = bendingConst*(F2(pos,dist,2,bAngle)+F3(pos,dist,2,bAngle));
         
     elseif pIdx == (numParticles-1)
-        force(pIdx,:) = bendingConst*((cosTheta(particlePosition,particleDistance,pIdx-2)-bAngle)*F1(particlePosition,particleDistance,pIdx)+...
-                                      (cosTheta(particlePosition,particleDistance,pIdx-1)-bAngle)*F2(particlePosition,particleDistance,pIdx));
-        
+%         force(pIdx,:) = bendingConst*((cosTheta(particlePosition,particleDistance,pIdx-2)-bAngle)*F1(particlePosition,particleDistance,pIdx)+...
+%                                       (cosTheta(particlePosition,particleDistance,pIdx-1)-bAngle)*F2(particlePosition,particleDistance,pIdx));
+          force(pIdx,:) = bendingConst*(F2(pos,dist,pIdx,bAngle)+F1(pos,dist,pIdx,bAngle));
     elseif pIdx == numParticles        
-        force(pIdx,:) = bendingConst*(cosTheta(particlePosition,particleDistance,pIdx-2)-bAngle)*F1(particlePosition,particleDistance,pIdx);        
+%         force(pIdx,:) = bendingConst*(cosTheta(particlePosition,particleDistance,pIdx-2)-bAngle)*F1(particlePosition,particleDistance,pIdx);        
+         force(pIdx,:) = bendingConst*(F1(pos,dist,pIdx,bAngle));
     else
-        
-        force(pIdx,:) = bendingConst*((cosTheta(particlePosition,particleDistance,pIdx-2)- bAngle)*F1(particlePosition,particleDistance,pIdx)+...
-                                      (cosTheta(particlePosition,particleDistance,pIdx-1)- bAngle)*F2(particlePosition,particleDistance,pIdx)+...
-                                      (cosTheta(particlePosition,particleDistance,pIdx)  - bAngle)*F3(particlePosition,particleDistance,pIdx));
+%         
+%         force(pIdx,:) = bendingConst*((cosTheta(particlePosition,particleDistance,pIdx-2)- bAngle)*F1(particlePosition,particleDistance,pIdx)+...
+%                                       (cosTheta(particlePosition,particleDistance,pIdx-1)- bAngle)*F2(particlePosition,particleDistance,pIdx)+...
+%                                       (cosTheta(particlePosition,particleDistance,pIdx)  - bAngle)*F3(particlePosition,particleDistance,pIdx));
+         force(pIdx,:)= bendingConst*(F1(pos,dist,pIdx,bAngle)+F2(pos,dist,pIdx,bAngle)+F3(pos,dist,pIdx,bAngle));
         
     end
     
