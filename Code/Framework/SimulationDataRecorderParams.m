7a0f9b15d931c5b79cd84c1f10ed30bdb53a2fad
zhaENS <barbarazha@hotmail.com>
2015-08-18 18:23:41 +0200

    commit

--- Code/Framework/SimulationDataRecorderParams.m
+++ Code/Framework/SimulationDataRecorderParams.m
@@ -16,8 +16,8 @@ classdef SimulationDataRecorderParams<handle
         function SetDefaultParams(obj)
             obj.saveType       = 'external'; %save to external mat file. no data is save in the class. options [external/internal/none/all]
             %obj.resultsFolder  = 'D:\Ofir\Work\PolymerChainDynamicsResults';
-            obj.resultsFolder =  'C:\projectsENS\PolymerChainDynamicsResults';
-            %obj.resultsFolder  = 'D:\Zha\Project\PolymerChainDynamicsResults';
+            %obj.resultsFolder =  'C:\projectsENS\PolymerChainDynamicsResults';
+             obj.resultsFolder  = 'D:\Zha\Project\PolymerChainDynamicsResults';
             obj.recipeFileName = '';%will be copied by framework class
             obj.encounterDist  = []; %  The distance for which two monomer are considered to have met
         end
